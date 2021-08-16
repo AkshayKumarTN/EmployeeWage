e6c8842350390e6bf8eff7f45f9faf2fd92b4d5e
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-16 20:51:45 +0530

    [Akshay] Added -> Function for Calculating Daily Employee Wage based on part time or full time work

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -11,7 +11,7 @@ else
     console.log("Employee is Absent");
 }
 
-console.log("------------------UC-2----------------------------");
+console.log("------------------UC-2--AND--UC-3-----------------------");
 // Calculating Daily Employee Wage based on part time or full time work...........
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -19,18 +19,20 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 3);
+function getWorkingHours(empCheck){
 switch (empCheck) {
     case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
+        return PART_TIME_HOURS;
         break;
     case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
+        return FULL_TIME_HOURS;
         break;
     default:
-        empHrs=0;
+        return 0;
 }
-
+}
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() *3);
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage : "+ empWage);
