9bc3ffb247fbccb93b95e68db425b26a75b47eef
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-17 09:18:15 +0530

    [Akshay] Added -> Method To Store Daily Wage In Array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,18 +1,3 @@
-console.log(" Employee Wage");
-console.log("------------------UC-1----------------------------");
-const IS_PRESENT=0;
-let attendance=Math.floor(Math.random() * 2);
-if(attendance==IS_PRESENT)
-{
-    console.log("Employee is Present");
-}
-else
-{
-    console.log("Employee is Absent");
-}
-
-console.log("------------------UC-5-----------------------");
-// Calculating Wages for a Month assuming 20 Working Days in a Month..........
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -20,25 +5,36 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
+let empDailyWageArray = new Array();
+
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
 
 function getWorkingHours(empCheck){
-switch (empCheck) {
-    case IS_PART_TIME:
-        return PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        return FULL_TIME_HOURS;
-        break;
-    default:
-        return 0;
+    switch (empCheck) 
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    }
 }
-}
-let empHrs = 0;
+let totalEmpHrs = 0;
 let totalWorkingDays = 0;
-while(empHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+{
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() *3);
-    empHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArray.push(empHrs)
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total working days : "+ totalWorkingDays +"\nTotal Hours : "+ empHrs +"\nEmp Wage : "+ empWage);
+// Calculating Wages for a Month assuming 20 Working Days in a Month..........
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total working days : "+ totalWorkingDays +"\nTotal Hours : "+ totalEmpHrs +"\nEmp Wage : "+ empWage);
+
