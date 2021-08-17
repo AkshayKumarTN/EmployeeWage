ba4304c07fa2537821d3e88d243134a1d0458d1a
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-17 09:06:54 +0530

    [Akshay] Added -> Max Working Hours Or Days Condition To Calculate Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -11,7 +11,7 @@ else
     console.log("Employee is Absent");
 }
 
-console.log("------------------UC-4-----------------------");
+console.log("------------------UC-5-----------------------");
 // Calculating Wages for a Month assuming 20 Working Days in a Month..........
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -19,6 +19,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck){
 switch (empCheck) {
@@ -33,9 +34,11 @@ switch (empCheck) {
 }
 }
 let empHrs = 0;
-for(let day = 0; day <NUM_OF_WORKING_DAYS; day++){
-let empCheck = Math.floor(Math.random() *3);
-empHrs += getWorkingHours(empCheck);
+let totalWorkingDays = 0;
+while(empHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() *3);
+    empHrs += getWorkingHours(empCheck);
 }
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hours : "+ empHrs +"\nEmp Wage : "+ empWage);
+console.log("Total working days : "+ totalWorkingDays +"\nTotal Hours : "+ empHrs +"\nEmp Wage : "+ empWage);
